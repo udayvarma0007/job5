531a68eeeab3752425bddf8e1fa6058f0762224e
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-07-31 19:35:12 +0530

    Add files via upload

---
+++ Batch-Job-37/src/Job37.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+public class Job37 {
+
+	public static void main(String[] args) {
+		try {
+        	String scriptPath = "/home/udayvarma/Batch-jobs/Batch-Job-26/scripts/skopeprocess.sh";
+        	
+            ProcessBuilder processBuilder = new ProcessBuilder("bash", scriptPath);
+            
+            
+            Process process = processBuilder.start();
+            
+           
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                System.out.println(" successfully.");
+            } else {
+                System.out.println("Failed .");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+	}
+
+}
